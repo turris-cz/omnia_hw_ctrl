0b0e1c70b10e9a2989ad9b26fd4b5e176fe39419
Jiri Kovanda <jiri.kovanda@nic.cz>
2015-07-31 09:29:37 +0200

    set power-up sequence

--- src/app/power_control.c
+++ src/app/power_control.c
@@ -13,7 +13,7 @@
 #include "delay.h"
 
 /* Private define ------------------------------------------------------------*/
-
+//#define USE_4V5_POWER
 /* Private functions ---------------------------------------------------------*/
 
 /*******************************************************************************
@@ -50,10 +50,10 @@ void power_control_io_config(void)
 
     GPIO_InitStructure.GPIO_Pin = ENABLE_1V35_PIN;
     GPIO_Init(ENABLE_1V35_PIN_PORT, &GPIO_InitStructure);
-
+#ifdef USE_4V5_POWER
     GPIO_InitStructure.GPIO_Pin = ENABLE_4V5_PIN;
     GPIO_Init(ENABLE_4V5_PIN_PORT, &GPIO_InitStructure);
-
+#endif
     GPIO_InitStructure.GPIO_Pin = ENABLE_1V8_PIN;
     GPIO_Init(ENABLE_1V8_PIN_PORT, &GPIO_InitStructure);
 
@@ -112,10 +112,10 @@ void power_control_io_config(void)
 
     GPIO_InitStructure.GPIO_Pin = PG_1V35_PIN;
     GPIO_Init(PG_1V35_PIN_PORT, &GPIO_InitStructure);
-
+#ifdef USE_4V5_POWER
     GPIO_InitStructure.GPIO_Pin = PG_4V5_PIN;
     GPIO_Init(PG_4V5_PIN_PORT, &GPIO_InitStructure);
-
+#endif
     GPIO_InitStructure.GPIO_Pin = PG_1V8_PIN;
     GPIO_Init(PG_1V8_PIN_PORT, &GPIO_InitStructure);
 
@@ -149,35 +149,47 @@ void power_control_io_config(void)
   *****************************************************************************/
 void power_control_enable_regulator(void)
 {
+    /*
+     * power-up sequence:
+     * 1) 5V regulator
+     *    3.3V regulator
+     * 2) 1.8V regulator
+     *    1.5V regulator
+     * 3) 1.35V regulator
+     *    VTT regulator
+     * 4) 4.5V regulator - if used
+     * 5) 1.2V regulator
+     */
     GPIO_SetBits(ENABLE_5V_PIN_PORT, ENABLE_5V_PIN);
     GPIO_SetBits(ENABLE_3V3_PIN_PORT, ENABLE_3V3_PIN);
-    GPIO_SetBits(ENABLE_1V35_PIN_PORT, ENABLE_1V35_PIN);
-    GPIO_SetBits(ENABLE_4V5_PIN_PORT, ENABLE_4V5_PIN);
-
     while(!(GPIO_ReadInputDataBit(PG_5V_PIN_PORT, PG_5V_PIN)))
         ;
     while(!(GPIO_ReadInputDataBit(PG_3V3_PIN_PORT, PG_3V3_PIN)))
         ;
-    while(!(GPIO_ReadInputDataBit(PG_1V35_PIN_PORT, PG_1V35_PIN)))
-        ;
-    while(!(GPIO_ReadInputDataBit(PG_4V5_PIN_PORT, PG_4V5_PIN)))
-        ;
 
     GPIO_SetBits(ENABLE_1V8_PIN_PORT, ENABLE_1V8_PIN);
     GPIO_SetBits(ENABLE_1V5_PIN_PORT, ENABLE_1V5_PIN);
-    GPIO_SetBits(ENABLE_VTT_PIN_PORT, ENABLE_VTT_PIN);
-
-
     while(!(GPIO_ReadInputDataBit(PG_1V8_PIN_PORT, PG_1V8_PIN)))
         ;
     while(!(GPIO_ReadInputDataBit(PG_1V5_PIN_PORT, PG_1V5_PIN)))
         ;
+
+
+
+    GPIO_SetBits(ENABLE_1V35_PIN_PORT, ENABLE_1V35_PIN);
+    GPIO_SetBits(ENABLE_VTT_PIN_PORT, ENABLE_VTT_PIN);
+    while(!(GPIO_ReadInputDataBit(PG_1V35_PIN_PORT, PG_1V35_PIN)))
+        ;
     while(!(GPIO_ReadInputDataBit(PG_VTT_PIN_PORT, PG_VTT_PIN)))
         ;
+
+#ifdef USE_4V5_POWER
+    GPIO_SetBits(ENABLE_4V5_PIN_PORT, ENABLE_4V5_PIN);
+    while(!(GPIO_ReadInputDataBit(PG_4V5_PIN_PORT, PG_4V5_PIN)))
+        ;
+#endif
 
     GPIO_SetBits(ENABLE_1V2_PIN_PORT, ENABLE_1V2_PIN);
     while(!(GPIO_ReadInputDataBit(PG_1V2_PIN_PORT, PG_1V2_PIN)))
         ;
-    //TODO - check it
-
 }
