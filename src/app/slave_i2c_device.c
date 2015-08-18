cd961f9b1363bae744e1a0de1ffc303b08d44441
Jiri Kovanda <jiri.kovanda@nic.cz>
2015-08-18 11:11:05 +0200

    add i2c initialization

--- src/app/slave_i2c_device.c
+++ src/app/slave_i2c_device.c
@@ -8,3 +8,79 @@
  ******************************************************************************
  **/
 /* Includes ------------------------------------------------------------------*/
+#include "stm32f0xx_conf.h"
+#include "slave_i2c_device.h"
+
+#define I2C_SDA_SOURCE                  GPIO_PinSource7
+#define I2C_SCL_SOURCE                  GPIO_PinSource6
+
+#define I2C2_ALTERNATE_FUNCTION         GPIO_AF_1
+#define I2C_TIMING                      0x1045061D
+
+#define I2C_GPIO_CLOCK                  RCC_AHBPeriph_GPIOF
+#define I2C_PERIPH_NAME                 I2C2
+#define I2C_PERIPH_CLOCK                RCC_APB1Periph_I2C2
+#define I2C_DATA_PIN                    GPIO_Pin_7 // I2C2_SDA - GPIOF
+#define I2C_CLK_PIN                     GPIO_Pin_6 // I2C2_SCL - GPIOF
+#define I2C_GPIO_PORT                   GPIOF
+
+struct st_i2c_status i2c_state;
+
+/*******************************************************************************
+  * @function   slave_i2c_config
+  * @brief      Configuration of I2C peripheral as a slave.
+  * @param      None.
+  * @retval     None.
+  *****************************************************************************/
+static void slave_i2c_config(void)
+{
+    GPIO_InitTypeDef  GPIO_InitStructure;
+    I2C_InitTypeDef  I2C_InitStructure;
+    NVIC_InitTypeDef NVIC_InitStructure;
+
+    /* I2C Periph clock enable */
+    RCC_APB1PeriphClockCmd(I2C_PERIPH_CLOCK, ENABLE);
+
+    RCC_AHBPeriphClockCmd(I2C_GPIO_CLOCK, ENABLE);
+
+    /* Connect PXx to I2C_SCL */
+    GPIO_PinAFConfig(I2C_GPIO_PORT, I2C_SCL_SOURCE, I2C2_ALTERNATE_FUNCTION);
+
+    /* Connect PXx to I2C_SDA */
+    GPIO_PinAFConfig(I2C_GPIO_PORT, I2C_SDA_SOURCE, I2C2_ALTERNATE_FUNCTION);
+
+    /* Configure I2C pins: SCL */
+    GPIO_InitStructure.GPIO_Pin = I2C_CLK_PIN;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_OD;
+    GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL;
+    GPIO_Init(I2C_GPIO_PORT, &GPIO_InitStructure);
+
+    /* Configure I2C pins: SDA */
+    GPIO_InitStructure.GPIO_Pin = I2C_DATA_PIN;
+    GPIO_Init(I2C_GPIO_PORT, &GPIO_InitStructure);
+
+    /* I2C configuration */
+    I2C_InitStructure.I2C_Mode = I2C_Mode_I2C;
+    I2C_InitStructure.I2C_AnalogFilter = I2C_AnalogFilter_Enable;
+    I2C_InitStructure.I2C_DigitalFilter = 0x00;
+    I2C_InitStructure.I2C_OwnAddress1 = 0x55;   //own address of STM32 - slave mode
+    I2C_InitStructure.I2C_Ack = I2C_Ack_Enable;
+    I2C_InitStructure.I2C_AcknowledgedAddress = I2C_AcknowledgedAddress_7bit;
+    I2C_InitStructure.I2C_Timing = I2C_TIMING; //TODO: check i2c speed !
+
+    /* Apply I2C configuration after enabling it */
+    I2C_Init(I2C_PERIPH_NAME, &I2C_InitStructure);
+
+    /* Address match interrupt */
+    I2C_ITConfig(I2C_PERIPH_NAME, I2C_IT_ADDRI | I2C_IT_RXI, ENABLE);
+
+    /* I2C Peripheral Enable */
+    I2C_Cmd(I2C_PERIPH_NAME, ENABLE);
+
+    NVIC_InitStructure.NVIC_IRQChannel = I2C2_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPriority = 0x01;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+}
