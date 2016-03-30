f32a300ccf5f0b17ea4f9b7267fb4f5fa0d78b3d
Jiri Kovanda <jiri.kovanda@nic.cz>
2016-03-30 10:49:29 +0200

    PCA9538: implement logic for read, write, config and polarity regs

--- src/app/pca9538_emu.c
+++ src/app/pca9538_emu.c
@@ -1,9 +1,9 @@
 /**
  ******************************************************************************
- * @file    pca9534_emu.c
+ * @file    pca9538_emu.c
  * @author  CZ.NIC, z.s.p.o.
  * @date    29-March-2016
- * @brief   Emulator of PCA9534 GPIO I2C expander.
+ * @brief   Emulator of PCA9538 GPIO I2C expander.
  ******************************************************************************
  ******************************************************************************
  **/
@@ -13,99 +13,246 @@
 
 #define REGISTER_LENGTH             8
 
-enum pca9534_commands {
+enum pca9538_commands {
     INPUT_PORT_REG                  = 0x00,
     OUTPUT_PORT_REG                 = 0x01,
     POLARITY_INV_REG                = 0x02,
     CONFIG_REG                      = 0x03,
 };
 
-enum pca9534_config_reg {
+enum pca9538_config_reg {
     CONFIG_OUTPUT                   = 0x00,
     CONFIG_INPUT                    = 0x01
 };
 
-enum pca9538_port_config {
-    PIN0_SFP_DET                    = 0x01,
-    PIN1_SFP_FLT                    = 0x02,
-    PIN2_SFP_LOST                   = 0x04,
-    PIN3_SFP_DIS                    = 0x08
+enum pca9538_pin_mask {
+    PIN0_SFP_DET_MASK               = 0x01,
+    PIN1_SFP_FLT_MASK               = 0x02,
+    PIN2_SFP_LOST_MASK              = 0x04,
+    PIN3_SFP_DIS_MASK               = 0x08
 };
 
-struct pca9534_st{
+typedef enum pca9538_pin_config {
+    PIN0_SFP_DET                    = 0x00,
+    PIN1_SFP_FLT                    = 0x01,
+    PIN2_SFP_LOST                   = 0x02,
+    PIN3_SFP_DIS                    = 0x03
+} pin_config_t;
+
+struct pca9538_st{
     uint8_t config_reg;
     uint8_t pol_inv_reg;
 };
 
-struct pca9534_st pca9534;
+static struct pca9538_st pca9538;
 
 /*******************************************************************************
-  * @function   wan_sfp_connector_detection
-  * @brief      Detect inserted SFP+ connector.
+  * @function   pca9534_read_input
+  * @brief      Read byte from input port.
   * @param      None.
-  * @retval     1 - SFP detected, 0 - SFP not detected.
+  * @retval     Input port state.
   *****************************************************************************/
 uint8_t pca9534_read_input(void)
 {
     uint8_t input_port = 0;
+    struct pca9538_st *expander = &pca9538;
 
-    input_port |= wan_sfp_connector_detection();
-    input_port |= (wan_sfp_fault_detection() << 1);
-    input_port |= (wan_sfp_lost_detection() << 2);
-    input_port |= (wan_sfp_get_tx_status() << 3);
+    /* check setting in polarity inversion register */
+    if (expander->pol_inv_reg & PIN0_SFP_DET_MASK)
+    {
+        input_port |= PIN0_SFP_DET_MASK & (~(wan_sfp_connector_detection()));
+    }
+    else
+    {
+        input_port |= wan_sfp_connector_detection();
+    }
+
+    if (expander->pol_inv_reg & PIN1_SFP_FLT_MASK)
+    {
+        input_port |= PIN1_SFP_FLT_MASK & (~(wan_sfp_fault_detection() << 1));
+    }
+    else
+    {
+        input_port |= (wan_sfp_fault_detection() << 1);
+    }
+
+    if (expander->pol_inv_reg & PIN2_SFP_LOST_MASK)
+    {
+        input_port |= PIN2_SFP_LOST_MASK & (~(wan_sfp_lost_detection() << 2));
+    }
+    else
+    {
+        input_port |= (wan_sfp_lost_detection() << 2);
+    }
+
+    if (expander->pol_inv_reg & PIN3_SFP_DIS_MASK)
+    {
+        input_port |= PIN3_SFP_DIS_MASK & (~(wan_sfp_get_tx_status() << 3));
+    }
+    else
+    {
+        input_port |= (wan_sfp_get_tx_status() << 3);
+    }
 
     return input_port;
 }
 
-void pca9534_write_output(uint8_t reg)
+/*******************************************************************************
+  * @function   pca9534_write_output
+  * @brief      Write byte to output register.
+  * @param      output_reg: value to be written to output register.
+  * @retval     None.
+  *****************************************************************************/
+void pca9534_write_output(uint8_t output_reg)
 {
-    uint8_t idx, mask = 0x01;
-    struct pca9534_st *expander = &pca9534;
-    uint8_t reg_masked;
+    uint8_t pin_value, config_reg_masked, mask = 0x01;
+    struct pca9538_st *expander = &pca9538;
+    pin_config_t pin;
 
-    for (idx = 0; idx < REGISTER_LENGTH; idx++)
+    for (pin = 0; pin < REGISTER_LENGTH; pin++)
     {
         config_reg_masked = expander->config_reg & mask;
         /* only output pins can be written */
         if((config_reg_masked) == CONFIG_OUTPUT)
         {
-            reg_masked = mask & reg;
+            pin_value = mask & output_reg;
 
-            switch(reg_masked)
+            switch(pin)
             {
                 case PIN0_SFP_DET:
                 {
-                    GPIO_WriteBit(SFP_DET_PIN_PORT, SFP_DET_PIN, reg_masked);
+                    GPIO_WriteBit(SFP_DET_PIN_PORT, SFP_DET_PIN, pin_value);
                 } break;
 
                 case PIN1_SFP_FLT:
                 {
-                    GPIO_WriteBit(SFP_FLT_PIN_PORT, SFP_FLT_PIN, reg_masked);
+                    GPIO_WriteBit(SFP_FLT_PIN_PORT, SFP_FLT_PIN, pin_value);
                 } break;
 
                 case PIN2_SFP_LOST:
                 {
-                    GPIO_WriteBit(SFP_LOS_PIN_PORT, SFP_LOS_PIN, reg_masked);
+                    GPIO_WriteBit(SFP_LOS_PIN_PORT, SFP_LOS_PIN, pin_value);
                 } break;
 
                 case PIN3_SFP_DIS:
                 {
-                    GPIO_WriteBit(SFP_DIS_PIN_PORT, SFP_DIS_PIN, reg_masked);
+                    GPIO_WriteBit(SFP_DIS_PIN_PORT, SFP_DIS_PIN, pin_value);
                 } break;
+
+                default:
+                    break;
             }
         }
-        mask <<= idx;
+        mask <<= 1;
     }
 }
 
-void pca9534_set_polarity_inv(void)
+/*******************************************************************************
+  * @function   pca9534_set_polarity_inv
+  * @brief      Write byte to polarity inversion register.
+  * @param      polarity: value to be written to polarity inversion register.
+  * @retval     None.
+  *****************************************************************************/
+void pca9534_set_polarity_inv(uint8_t polarity)
 {
+    struct pca9538_st *expander = &pca9538;
 
+    expander->pol_inv_reg = polarity;
 }
 
-void pca9534_set_config(uint8_t reg)
+/*******************************************************************************
+  * @function   pca9534_set_config
+  * @brief      Write byte to configuration register and configure pins.
+  * @param      config_reg: value to be written to configuration register.
+  * @retval     None.
+  *****************************************************************************/
+void pca9534_set_config(uint8_t config_reg)
 {
-    struct pca9534_st *expander = &pca9534;
+    struct pca9538_st *expander = &pca9538;
+    pin_config_t pin;
+    uint8_t config_reg_masked, mask = 0x01;
+    GPIO_InitTypeDef  GPIO_InitStructure;
 
-    expander->config_reg = reg;
+    expander->config_reg = config_reg;
+
+    for (pin = 0; pin < REGISTER_LENGTH; pin++)
+    {
+        config_reg_masked = config_reg & mask;
+
+        if (config_reg_masked == CONFIG_OUTPUT) /* pin configured as output */
+        {
+            GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+            GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+            GPIO_InitStructure.GPIO_Speed = GPIO_Speed_Level_1;
+            GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+
+            switch(pin)
+            {
+                case PIN0_SFP_DET:
+                {
+                    GPIO_InitStructure.GPIO_Pin = SFP_DET_PIN;
+                    GPIO_Init(SFP_DET_PIN_PORT, &GPIO_InitStructure);
+                } break;
+
+                case PIN1_SFP_FLT:
+                {
+                    GPIO_InitStructure.GPIO_Pin = SFP_FLT_PIN;
+                    GPIO_Init(SFP_FLT_PIN_PORT, &GPIO_InitStructure);
+                } break;
+
+                case PIN2_SFP_LOST:
+                {
+                    GPIO_InitStructure.GPIO_Pin = SFP_LOS_PIN;
+                    GPIO_Init(SFP_LOS_PIN_PORT, &GPIO_InitStructure);
+                } break;
+
+                case PIN3_SFP_DIS:
+                {
+                    GPIO_InitStructure.GPIO_Pin = SFP_DIS_PIN;
+                    GPIO_Init(SFP_DIS_PIN_PORT, &GPIO_InitStructure);
+                } break;
+
+                default:
+                    break;
+            }
+        }
+        else /* pin configured as input */
+        {
+            GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+            GPIO_InitStructure.GPIO_Speed = GPIO_Speed_Level_1;
+            GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+
+            switch(pin)
+            {
+                case PIN0_SFP_DET:
+                {
+                    GPIO_InitStructure.GPIO_Pin = SFP_DET_PIN;
+                    GPIO_Init(SFP_DET_PIN_PORT, &GPIO_InitStructure);
+                } break;
+
+                case PIN1_SFP_FLT:
+                {
+                    GPIO_InitStructure.GPIO_Pin = SFP_FLT_PIN;
+                    GPIO_Init(SFP_FLT_PIN_PORT, &GPIO_InitStructure);
+                } break;
+
+                case PIN2_SFP_LOST:
+                {
+                    GPIO_InitStructure.GPIO_Pin = SFP_LOS_PIN;
+                    GPIO_Init(SFP_LOS_PIN_PORT, &GPIO_InitStructure);
+                } break;
+
+                case PIN3_SFP_DIS:
+                {
+                    GPIO_InitStructure.GPIO_Pin = SFP_DIS_PIN;
+                    GPIO_Init(SFP_DIS_PIN_PORT, &GPIO_InitStructure);
+                } break;
+
+                default:
+                    break;
+            }
+        }
+
+        mask <<= 1;
+    }
 }
--- src/app/pca9538_emu.h
+++ src/app/pca9538_emu.h
@@ -1,13 +1,19 @@
 /**
  ******************************************************************************
- * @file    pca9534_emu.h
+ * @file    pca9538_emu.h
  * @author  CZ.NIC, z.s.p.o.
  * @date    29-March-2016
- * @brief   Header for emulation of PCA9534 expander.
+ * @brief   Header for emulation of PCA9538 expander.
  ******************************************************************************
  ******************************************************************************
  **/
-#ifndef PCA9534_EMULATOR_H
-#define PCA9534_EMULATOR_H
+#ifndef PCA9538_EMULATOR_H
+#define PCA9538_EMULATOR_H
 
-#endif /* PCA9534_EMULATOR_H */
+#define PCA9538_PIN_DEFAULT_CONFIG       0xFF /* default value for config reg */
+
+void pca9534_set_config(uint8_t reg);
+void pca9534_write_output(uint8_t reg);
+uint8_t pca9534_read_input(void);
+
+#endif /* PCA9538_EMULATOR_H */
