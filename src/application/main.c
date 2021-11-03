d2e018772d9a75fe8a85b7e3bd2d045ebffa1de9
Jiri Kovanda <jiri.kovanda@nic.cz>
2021-11-03 11:52:24 +0100

    update main.c

--- src/application/main.c
+++ src/application/main.c
@@ -1,108 +1,92 @@
-/*!
-    \file  main.c
-    \brief GPIO running led
+/**
+ ******************************************************************************
+ * @file    main.c
+ * @author  CZ.NIC, z.s.p.o.
+ * @date    21-July-2015
+ * @brief   Main program body
+ ******************************************************************************
+ ******************************************************************************
+ **/
 
-    \version 2016-01-15, V1.0.0, demo for GD32F1x0
-    \version 2016-05-13, V2.0.0, demo for GD32F1x0
-    \version 2019-11-20, V3.0.0, demo for GD32F1x0
-*/
-
-/*
-    Copyright (c) 2019, GigaDevice Semiconductor Inc.
-
-    Redistribution and use in source and binary forms, with or without modification, 
-are permitted provided that the following conditions are met:
-
-    1. Redistributions of source code must retain the above copyright notice, this 
-       list of conditions and the following disclaimer.
-    2. Redistributions in binary form must reproduce the above copyright notice, 
-       this list of conditions and the following disclaimer in the documentation 
-       and/or other materials provided with the distribution.
-    3. Neither the name of the copyright holder nor the names of its contributors 
-       may be used to endorse or promote products derived from this software without 
-       specific prior written permission.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
-NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
-PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
-WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY 
-OF SUCH DAMAGE.
-*/
-
+/* Includes ------------------------------------------------------------------*/
 #include "gd32f1x0.h"
-#include "delay.h"
-#include "power_control.h"
-#include "msata_pci.h"
-#include "wan_lan_pci_status.h"
-#include "led_driver.h"
-#include "slave_i2c_device.h"
-#include "debug_serial.h"
+#include "app.h"
 
+#define APPLICATION_ADDRESS         0x08005000
+#define RAM_ADDRESS                 0x20000000
+#define BOOTLOADER_ADDRESS          0x08000000
 
-void rcu_config(void);
-void led_config(void);
+typedef void (*pFunction)(void);
 
-/*!
-    \brief      main function
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
+void start_bootloader(void);
+
+/*******************************************************************************
+ * @brief  Main program.
+ * @param  None
+ * @retval None
+ ******************************************************************************/
 int main(void)
 {
-    rcu_config();
-    led_config();
+//    /* Copy interrupt vector table to the RAM. */
+//    volatile uint32_t *vector_table = (volatile uint32_t *)RAM_ADDRESS;
+//    uint32_t vector_index = 0;
 
-    delay_systimer_config();
-    power_control_io_config();
-    msata_pci_indication_config();
-    wan_lan_pci_config();
-    power_control_usb_timeout_config();
-    led_driver_config();
-    slave_i2c_config();
-    debug_serial_config();
+//    for(vector_index = 0; vector_index < 48; vector_index++)
+//    {
+//        vector_table[vector_index] = *(volatile uint32_t*)((uint32_t)APPLICATION_ADDRESS + (vector_index << 2));
+//    }
 
-    while(1){
-        gpio_bit_set(GPIOF, GPIO_PIN_6);
-        /* delay 200ms */
-        delay(200);
-        gpio_bit_set(GPIOF, GPIO_PIN_7);
-        /* delay 200ms */
-        delay(200);
-        /* toggle LEDs */
-        gpio_bit_write(GPIOF, GPIO_PIN_6, (bit_status)((1 - gpio_output_bit_get(GPIOF, GPIO_PIN_6))));        
-        gpio_bit_write(GPIOF, GPIO_PIN_7, (bit_status)((1 - gpio_output_bit_get(GPIOF, GPIO_PIN_7))));
-        /* delay 200ms */
-        delay(200);
+//    /* force AHB reset */
+//    RCC_AHBPeriphResetCmd(RCC_AHBPeriph_GPIOA | RCC_AHBPeriph_GPIOB | \
+//        RCC_AHBPeriph_GPIOC | RCC_AHBPeriph_GPIOD | RCC_AHBPeriph_GPIOF, ENABLE);
+
+//    /* Enable the SYSCFG peripheral clock*/
+//    RCC_APB2PeriphResetCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+
+//    /* release AHB reset */
+//    RCC_AHBPeriphResetCmd(RCC_AHBPeriph_GPIOA | RCC_AHBPeriph_GPIOB | \
+//        RCC_AHBPeriph_GPIOC | RCC_AHBPeriph_GPIOD | RCC_AHBPeriph_GPIOF, DISABLE);
+
+//    RCC_APB2PeriphResetCmd(RCC_APB2Periph_SYSCFG, DISABLE);
+
+//    /* Remap SRAM */
+//    SYSCFG_MemoryRemapConfig(SYSCFG_MemoryRemap_SRAM);
+
+//    __enable_irq();
+
+    app_mcu_init();
+
+    while(1)
+    {
+        app_mcu_cyclic();
     }
 }
 
-/*!
-    \brief      configure LEDs
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
-void led_config(void)
+void start_bootloader(void)
 {
-    /* configure LED GPIO port */ 
-    gpio_mode_set(GPIOF, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_PIN_6 | GPIO_PIN_7);
-    gpio_output_options_set(GPIOF, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_6 | GPIO_PIN_7);
-    gpio_bit_reset(GPIOF, GPIO_PIN_6 | GPIO_PIN_7);
-}
+    pFunction boot_entry;
+    uint32_t boot_stack;
 
-/*!
-    \brief      clock configuration
-    \param[in]  none
-    \param[out] none
-    \retval     none
-*/
-void rcu_config(void)
-{
-    rcu_periph_clock_enable(RCU_GPIOF);
+    __disable_irq();
+
+    /* Get the Bootloader stack pointer (First entry in the Bootloader vector table) */
+    boot_stack = (uint32_t) *((volatile uint32_t*)BOOTLOADER_ADDRESS);
+
+    /* Get the Bootloader entry point (Second entry in the Bootloader vector table) */
+    boot_entry = (pFunction) *(volatile uint32_t*) (BOOTLOADER_ADDRESS + 4);
+
+    /* Set the Bootloader stack pointer */
+    __set_MSP(boot_stack);
+
+        /* ISB = instruction synchronization barrier. It flushes the pipeline of
+     * the processor, so that all instructions following the ISB are fetched
+     * from cache or memory again, after the ISB instruction has been completed.
+     * Must be called after changing stack pointer according to the documentation.
+    */
+    __ISB();
+
+    /* Start the Bootloader */
+    boot_entry();
+
+    while(1);
 }
