c6d1e10331e9b030b456b740cc1f7f7ef0197801
Marek Behún <marek.behun@nic.cz>
2022-07-25 11:07:57 +0200

    compiler.h: change indentation to tabs
    Signed-off-by: Marek Behún <marek.behun@nic.cz>

--- src/application/app/compiler.h
+++ src/application/app/compiler.h
@@ -3,15 +3,15 @@
 
 #define ARRAY_SIZE(__x) (sizeof((__x)) / sizeof((__x)[0]))
 
-#define for_each(__m, __a)			\
-    for (typeof((__a)[0]) *(__m) = &(__a)[0];	\
-         (__m) < &(__a)[ARRAY_SIZE((__a))];	\
-         ++(__m))
+#define for_each(__m, __a)				\
+	for (typeof((__a)[0]) *(__m) = &(__a)[0];	\
+	     (__m) < &(__a)[ARRAY_SIZE((__a))];		\
+	     ++(__m))
 
 #define for_each_const(__m, __a)			\
-    for (const typeof((__a)[0]) *(__m) = &(__a)[0];	\
-         (__m) < &(__a)[ARRAY_SIZE((__a))];		\
-         ++(__m))
+	for (const typeof((__a)[0]) *(__m) = &(__a)[0];	\
+	     (__m) < &(__a)[ARRAY_SIZE((__a))];		\
+	      ++(__m))
 
 #define unreachable() __builtin_unreachable()
 
