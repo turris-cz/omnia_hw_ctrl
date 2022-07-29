31fdb9a36c661c8851cd2545ff997760ed1d58b2
Marek Behún <marek.behun@nic.cz>
2022-07-29 12:00:56 +0200

    Drop unused interrupt handlers
    Signed-off-by: Marek Behún <marek.behun@nic.cz>

--- src/platform/startup.c
+++ src/platform/startup.c
@@ -39,31 +39,11 @@ void pendsv_handler(void) __weak_alias(default_handler);
 void external_irq(void) __weak_alias(default_handler);
 
 void systick_irq_handler(void) __weak_alias(default_handler);
-void WWDG_IRQHandler(void) __weak_alias(default_handler);
-void RTC_IRQHandler(void) __weak_alias(default_handler);
-void FLASH_IRQHandler(void) __weak_alias(default_handler);
-void RCC_IRQHandler(void) __weak_alias(default_handler);
-void EXTI0_1_IRQHandler(void) __weak_alias(default_handler);
-void EXTI2_3_IRQHandler(void) __weak_alias(default_handler);
-void EXTI4_15_IRQHandler(void) __weak_alias(default_handler);
-void DMA1_Channel1_IRQHandler(void) __weak_alias(default_handler);
-void DMA1_Channel2_3_IRQHandler(void) __weak_alias(default_handler);
-void DMA1_Channel4_5_IRQHandler(void) __weak_alias(default_handler);
-void ADC1_IRQHandler(void) __weak_alias(default_handler);
-void TIM1_BRK_UP_TRG_COM_IRQHandler(void) __weak_alias(default_handler);
-void TIM1_CC_IRQHandler(void) __weak_alias(default_handler);
 void led_driver_irq_handler(void) __weak_alias(default_handler);
 void led_driver_effect_irq_handler(void) __weak_alias(default_handler);
-void TIM14_IRQHandler(void) __weak_alias(default_handler);
-void TIM15_IRQHandler(void) __weak_alias(default_handler);
 void debounce_timer_irq_handler(void) __weak_alias(default_handler);
 void power_control_usb_timeout_irq_handler(void) __weak_alias(default_handler);
-void I2C1_IRQHandler(void) __weak_alias(default_handler);
 void i2c_slave_irq_handler(void) __weak_alias(default_handler);
-void SPI1_IRQHandler(void) __weak_alias(default_handler);
-void SPI2_IRQHandler(void) __weak_alias(default_handler);
-void USART1_IRQHandler(void) __weak_alias(default_handler);
-void USART2_IRQHandler(void) __weak_alias(default_handler);
 
 static __used __section(".isr_vector") void *isr_vector[] = {
 	&_stack_top,
@@ -82,36 +62,36 @@ static __used __section(".isr_vector") void *isr_vector[] = {
 	NULL,
 	pendsv_handler,
 	systick_irq_handler,
-	WWDG_IRQHandler,		/* Window WatchDog */
-	NULL,				/* Reserved */
-	RTC_IRQHandler,			/* RTC through the EXTI line */
-	FLASH_IRQHandler,		/* FLASH */
-	RCC_IRQHandler,			/* RCC */
-	EXTI0_1_IRQHandler,		/* EXTI Line 0 and 1 */
-	EXTI2_3_IRQHandler,		/* EXTI Line 2 and 3 */
-	EXTI4_15_IRQHandler,		/* EXTI Line 4 to 15 */
-	NULL,				/* Reserved */
-	DMA1_Channel1_IRQHandler,	/* DMA1 Channel 1 */
-	DMA1_Channel2_3_IRQHandler,	/* DMA1 Channel 2 and Channel 3 */
-	DMA1_Channel4_5_IRQHandler,	/* DMA1 Channel 4 and Channel 5 */
-	ADC1_IRQHandler,		/* ADC1 */
-	TIM1_BRK_UP_TRG_COM_IRQHandler,	/* TIM1 Break, Update, Trigger and Commutation */
-	TIM1_CC_IRQHandler,		/* TIM1 Capture Compare */
-	NULL,				/* Reserved */
-	led_driver_irq_handler,		/* TIM3 */
-	led_driver_effect_irq_handler,	/* TIM6 */
-	NULL,				/* Reserved */
-	TIM14_IRQHandler,		/* TIM14 */
-	TIM15_IRQHandler,		/* TIM15 */
-	debounce_timer_irq_handler,	/* TIM16 */
-	power_control_usb_timeout_irq_handler, /* TIM17 */
-	I2C1_IRQHandler,		/* I2C1 */
-	i2c_slave_irq_handler,		/* I2C2 */
-	SPI1_IRQHandler,		/* SPI1 */
-	SPI2_IRQHandler,		/* SPI2 */
-	USART1_IRQHandler,		/* USART1 */
-	USART2_IRQHandler,		/* USART2 */
-	NULL,				/* Reserved */
-	NULL,				/* Reserved */
-	NULL,				/* Reserved */
+	NULL,					/* Window WatchDog */
+	NULL,					/* Reserved */
+	NULL,					/* RTC through the EXTI line */
+	NULL,					/* FLASH */
+	NULL,					/* RCC */
+	NULL,					/* EXTI Line 0 and 1 */
+	NULL,					/* EXTI Line 2 and 3 */
+	NULL,					/* EXTI Line 4 to 15 */
+	NULL,					/* Reserved */
+	NULL,					/* DMA1 Channel 1 */
+	NULL,					/* DMA1 Channel 2 and Channel 3 */
+	NULL,					/* DMA1 Channel 4 and Channel 5 */
+	NULL,					/* ADC1 */
+	NULL,					/* TIM1 Break, Update, Trigger and Commutation */
+	NULL,					/* TIM1 Capture Compare */
+	NULL,					/* Reserved */
+	led_driver_irq_handler,			/* TIM3 */
+	led_driver_effect_irq_handler,		/* TIM6 */
+	NULL,					/* Reserved */
+	NULL,					/* TIM14 */
+	NULL,					/* TIM15 */
+	debounce_timer_irq_handler,		/* TIM16 */
+	power_control_usb_timeout_irq_handler,	/* TIM17 */
+	NULL,					/* I2C1 */
+	i2c_slave_irq_handler,			/* I2C2 */
+	NULL,					/* SPI1 */
+	NULL,					/* SPI2 */
+	NULL,					/* USART1 */
+	NULL,					/* USART2 */
+	NULL,					/* Reserved */
+	NULL,					/* Reserved */
+	NULL,					/* Reserved */
 };
