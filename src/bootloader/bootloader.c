5f4c0ceea2abecee9d8a77a09af756e1c64ba3a1
Marek Behún <marek.behun@nic.cz>
2022-08-30 14:28:59 +0200

    bootloader: Indent with tabs
    Signed-off-by: Marek Behún <marek.behun@nic.cz>

--- src/bootloader/bootloader.c
+++ src/bootloader/bootloader.c
@@ -23,20 +23,20 @@
 #include "crc32.h"
 
 typedef enum {
-    POWER_ON,
-    STARTUP_MANAGER,
-    RESET_MANAGER,
-    FLASH_MANAGER,
-    START_APPLICATION,
-    RESET_TO_APPLICATION
+	POWER_ON,
+	STARTUP_MANAGER,
+	RESET_MANAGER,
+	FLASH_MANAGER,
+	START_APPLICATION,
+	RESET_TO_APPLICATION
 } boot_state_t;
 
 typedef enum {
-    GO_TO_POWER_ON,
-    GO_TO_STARTUP_MANAGER,
-    GO_TO_RESET_MANAGER,
-    GO_TO_FLASH,
-    GO_TO_APPLICATION
+	GO_TO_POWER_ON,
+	GO_TO_STARTUP_MANAGER,
+	GO_TO_RESET_MANAGER,
+	GO_TO_FLASH,
+	GO_TO_APPLICATION
 } boot_value_t;
 
 /*******************************************************************************
@@ -47,24 +47,24 @@ typedef enum {
   *****************************************************************************/
 static void bootloader_init(void)
 {
-    debug_init();
+	debug_init();
 
-    /* peripheral initialization*/
-    time_config();
-    led_driver_config();
-    boot_i2c_config();
+	/* peripheral initialization*/
+	time_config();
+	led_driver_config();
+	boot_i2c_config();
 
-    flash_init(); /* Unlock the Flash Program Erase controller */
-    EE_Init(); /* EEPROM Init */
-    timer_deinit(USB_TIMEOUT_TIMER);
-    enable_irq();
+	flash_init(); /* Unlock the Flash Program Erase controller */
+	EE_Init(); /* EEPROM Init */
+	timer_deinit(USB_TIMEOUT_TIMER);
+	enable_irq();
 
-    led_set_color24(LED_COUNT, GREEN_COLOR);
-    led_driver_reset_effect(ENABLE);
+	led_set_color24(LED_COUNT, GREEN_COLOR);
+	led_driver_reset_effect(ENABLE);
 
-    gpio_init_outputs(pin_opendrain, pin_spd_2, 1, SYSRES_OUT_PIN); /* dont control this ! */
+	gpio_init_outputs(pin_opendrain, pin_spd_2, 1, SYSRES_OUT_PIN); /* dont control this ! */
 
-    debug("Init\n");
+	debug("Init\n");
 }
 
 static bool check_app_crc(void)
@@ -104,69 +104,69 @@ static bool check_app_crc(void)
   *****************************************************************************/
 static boot_value_t startup_manager(void)
 {
-    eeprom_var_t ee_var;
-    uint16_t ee_data;
-    boot_value_t retval = GO_TO_RESET_MANAGER;
+	eeprom_var_t ee_var;
+	uint16_t ee_data;
+	boot_value_t retval = GO_TO_RESET_MANAGER;
 
-    ee_var = EE_ReadVariable(RESET_VIRT_ADDR, &ee_data);
+	ee_var = EE_ReadVariable(RESET_VIRT_ADDR, &ee_data);
 
-    switch(ee_var)
-    {
-        /* power on reset - first boot - everything is flashed;
-           request for reflashing has never ocurred */
-        case VAR_NOT_FOUND:
-        {
-            retval = GO_TO_APPLICATION;
+	switch(ee_var)
+	{
+		/* power on reset - first boot - everything is flashed;
+		   request for reflashing has never ocurred */
+		case VAR_NOT_FOUND:
+		{
+			retval = GO_TO_APPLICATION;
 
-            debug("R1\n");
-        } break;
+			debug("R1\n");
+		} break;
 
-        case VAR_FOUND:
-        {
-            switch (ee_data)
-            {
-                case BOOTLOADER_REQ:
-                {
-                    retval = GO_TO_FLASH;
-                    debug("req\n");
-                } break;
+		case VAR_FOUND:
+		{
+			switch (ee_data)
+			{
+				case BOOTLOADER_REQ:
+				{
+					retval = GO_TO_FLASH;
+					debug("req\n");
+				} break;
 
-                case FLASH_NOT_CONFIRMED: /* error */
-                {
-                    retval = GO_TO_POWER_ON;
-                    debug("ERR\n");
-                } break;
+				case FLASH_NOT_CONFIRMED: /* error */
+				{
+					retval = GO_TO_POWER_ON;
+					debug("ERR\n");
+				} break;
 
-                case FLASH_CONFIRMED: /* application was flashed correctly */
-                {
-                    retval = GO_TO_APPLICATION;
-                    debug("R2\n");
-                } break;
+				case FLASH_CONFIRMED: /* application was flashed correctly */
+				{
+					retval = GO_TO_APPLICATION;
+					debug("R2\n");
+				} break;
 
-                /* flag has not been saved correctly */
-                default: retval = GO_TO_POWER_ON; break;
-            }
-        } break;
+				/* flag has not been saved correctly */
+				default: retval = GO_TO_POWER_ON; break;
+			}
+		} break;
 
-        case VAR_NO_VALID_PAGE :
-        {
-            retval = GO_TO_POWER_ON;
-            debug("Boot-No valid page\n");
-        }
-            break;
+		case VAR_NO_VALID_PAGE :
+		{
+			retval = GO_TO_POWER_ON;
+			debug("Boot-No valid page\n");
+		}
+			break;
 
-        default:
-            break;
-    }
+		default:
+			break;
+	}
 
-    /*
-     * if EEprom variable says we should boot application but application has
-     * bad CRC, just power on
-     */
-    if (retval == GO_TO_APPLICATION && !check_app_crc())
-        retval = GO_TO_POWER_ON;
+	/*
+	 * if EEprom variable says we should boot application but application has
+	 * bad CRC, just power on
+	 */
+	if (retval == GO_TO_APPLICATION && !check_app_crc())
+		retval = GO_TO_POWER_ON;
 
-    return retval;
+	return retval;
 }
 
 /*******************************************************************************
@@ -177,125 +177,125 @@ static boot_value_t startup_manager(void)
   *****************************************************************************/
 static void bootloader(void)
 {
-    static boot_state_t next_state = STARTUP_MANAGER;
-    static boot_value_t val = GO_TO_RESET_MANAGER;
-    static flash_i2c_state_t flash_sts = FLASH_CMD_NOT_RECEIVED;
-    static uint8_t flash_confirmed;
-    static uint8_t power_supply_failure; /* if power supply disconnection occurred */
-    uint8_t system_reset;
+	static boot_state_t next_state = STARTUP_MANAGER;
+	static boot_value_t val = GO_TO_RESET_MANAGER;
+	static flash_i2c_state_t flash_sts = FLASH_CMD_NOT_RECEIVED;
+	static uint8_t flash_confirmed;
+	static uint8_t power_supply_failure; /* if power supply disconnection occurred */
+	uint8_t system_reset;
 
-    switch(next_state)
-    {
-        case STARTUP_MANAGER:
-        {
-            val = startup_manager();
+	switch(next_state)
+	{
+		case STARTUP_MANAGER:
+		{
+			val = startup_manager();
 
-            switch (val)
-            {
-                case GO_TO_POWER_ON:
-                {
-                    EE_WriteVariable(RESET_VIRT_ADDR, FLASH_NOT_CONFIRMED);
-                    next_state = POWER_ON;
-                } break;
-                case GO_TO_APPLICATION:
-                {
-                    next_state = START_APPLICATION;
-                } break;
-                case GO_TO_FLASH:
-                {
-                    EE_WriteVariable(RESET_VIRT_ADDR, FLASH_NOT_CONFIRMED);
-                    next_state = FLASH_MANAGER;
-                } break;
+			switch (val)
+			{
+				case GO_TO_POWER_ON:
+				{
+					EE_WriteVariable(RESET_VIRT_ADDR, FLASH_NOT_CONFIRMED);
+					next_state = POWER_ON;
+				} break;
+				case GO_TO_APPLICATION:
+				{
+					next_state = START_APPLICATION;
+				} break;
+				case GO_TO_FLASH:
+				{
+					EE_WriteVariable(RESET_VIRT_ADDR, FLASH_NOT_CONFIRMED);
+					next_state = FLASH_MANAGER;
+				} break;
 
-                default: next_state = POWER_ON;    break;
-            }
-        } break;
+				default: next_state = POWER_ON;    break;
+			}
+		} break;
 
-        case POWER_ON:
-        {
-            power_control_io_config();
-            power_control_set_startup_condition();
-            power_control_disable_regulators();
-            msleep(100);
+		case POWER_ON:
+		{
+			power_control_io_config();
+			power_control_set_startup_condition();
+			power_control_disable_regulators();
+			msleep(100);
 
-            power_control_enable_regulators();
-            power_control_first_startup();
-            power_supply_failure = 1;
-            next_state = FLASH_MANAGER;
-        } break;
+			power_control_enable_regulators();
+			power_control_first_startup();
+			power_supply_failure = 1;
+			next_state = FLASH_MANAGER;
+		} break;
 
-        case FLASH_MANAGER:
-        {
-            flash_sts = boot_i2c_flash_data();
+		case FLASH_MANAGER:
+		{
+			flash_sts = boot_i2c_flash_data();
 
-            switch(flash_sts)
-            {
-                case FLASH_CMD_RECEIVED: /* flashing has just started */
-                {
-                    next_state = RESET_MANAGER;
-                } break;
+			switch(flash_sts)
+			{
+				case FLASH_CMD_RECEIVED: /* flashing has just started */
+				{
+					next_state = RESET_MANAGER;
+				} break;
 
-                case FLASH_CMD_NOT_RECEIVED: /* nothing has received */
-                {
-                    next_state = RESET_MANAGER;
-                } break;
+				case FLASH_CMD_NOT_RECEIVED: /* nothing has received */
+				{
+					next_state = RESET_MANAGER;
+				} break;
 
-                case FLASH_WRITE_OK: /* flashing was successfull */
-                {
-                    if (!flash_confirmed)
-                    {
-                        EE_WriteVariable(RESET_VIRT_ADDR, FLASH_CONFIRMED);
-                        flash_confirmed = 1;
-                    }
+				case FLASH_WRITE_OK: /* flashing was successfull */
+				{
+					if (!flash_confirmed)
+					{
+						EE_WriteVariable(RESET_VIRT_ADDR, FLASH_CONFIRMED);
+						flash_confirmed = 1;
+					}
 
-                    next_state = RESET_MANAGER;
-                    debug("F_CONF\n");
-                } break;
+					next_state = RESET_MANAGER;
+					debug("F_CONF\n");
+				} break;
 
-                case FLASH_WRITE_ERROR: /* flashing was corrupted */
-                {
-                    /* flag FLASH_NOT_CONFIRMED is already set */
-                    next_state = RESET_MANAGER;
-                } break;
-            }
-        } break;
+				case FLASH_WRITE_ERROR: /* flashing was corrupted */
+				{
+					/* flag FLASH_NOT_CONFIRMED is already set */
+					next_state = RESET_MANAGER;
+				} break;
+			}
+		} break;
 
-        case RESET_MANAGER:
-        {
-            system_reset = gpio_read(SYSRES_OUT_PIN);
+		case RESET_MANAGER:
+		{
+			system_reset = gpio_read(SYSRES_OUT_PIN);
 
-            if(system_reset == 0) /* reset is active in low level */
-            {
-                next_state = RESET_TO_APPLICATION;
-            }
-            else
-            {
-                next_state = FLASH_MANAGER;
-            }
-        } break;
+			if(system_reset == 0) /* reset is active in low level */
+			{
+				next_state = RESET_TO_APPLICATION;
+			}
+			else
+			{
+				next_state = FLASH_MANAGER;
+			}
+		} break;
 
-        case START_APPLICATION:
-        {
-            reset_to_address(APPLICATION_BEGIN);
-        } break;
+		case START_APPLICATION:
+		{
+			reset_to_address(APPLICATION_BEGIN);
+		} break;
 
-        case RESET_TO_APPLICATION:
-        {
-            /* power supply wasnt disconnected and no command for flashing was received */
-            if ((power_supply_failure == 0) && (flash_sts == FLASH_CMD_NOT_RECEIVED))
-            {
-                /* we have old, but valid FW */
-                EE_WriteVariable(RESET_VIRT_ADDR, FLASH_CONFIRMED);
-            }
+		case RESET_TO_APPLICATION:
+		{
+			/* power supply wasnt disconnected and no command for flashing was received */
+			if ((power_supply_failure == 0) && (flash_sts == FLASH_CMD_NOT_RECEIVED))
+			{
+				/* we have old, but valid FW */
+				EE_WriteVariable(RESET_VIRT_ADDR, FLASH_CONFIRMED);
+			}
 
-            /* shutdown regulators before reset, otherwise power supply can
-            * stay there and causes wrong detection of mmc during boot */
-            power_control_set_startup_condition();
-            power_control_disable_regulators();
-            msleep(100);
-            NVIC_SystemReset();
-        } break;
-    }
+			/* shutdown regulators before reset, otherwise power supply can
+			* stay there and causes wrong detection of mmc during boot */
+			power_control_set_startup_condition();
+			power_control_disable_regulators();
+			msleep(100);
+			NVIC_SystemReset();
+		} break;
+	}
 }
 
 void main(void)
--- src/bootloader/bootloader.h
+++ src/bootloader/bootloader.h
@@ -12,9 +12,9 @@
 #define BOOTLOADER_H
 
 enum eeprom_flags {
-    BOOTLOADER_REQ                      = 0xAA,
-    FLASH_NOT_CONFIRMED                 = 0x55,
-    FLASH_CONFIRMED                     = 0x88
+	BOOTLOADER_REQ		= 0xAA,
+	FLASH_NOT_CONFIRMED	= 0x55,
+	FLASH_CONFIRMED		= 0x88
 };
 
 #endif /* BOOTLOADER_H */
