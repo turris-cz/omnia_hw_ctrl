turris-cz/omnia_hw_ctrl

commit d584227619bdebd9d5588e5febbf1f9917e8dc4a

drivers/led_driver: Use more color levels when gamma correcting
Since we have a nicely optimized frame computing code (~163 cycles on
Cortex-M0, ~138 cycles on Cortex-M3), we can call it more often.

This is useful when doing gamma correction of colors: when using only
256 color levels, with gamma correction the low value (0 - 10) grow very
slowly, meaning that changing color from #020300 to #050a00 does not
change the intensity of the LEDs at all.

Let's increase color levels 2.5 times on STM32 and 4 times on GD32
(since it is faster). Do this only when gamma correction is enabled.

Signed-off-by: Marek Behún <marek.behun@nic.cz>

src/drivers/led_driver.c
@@ -23,10 +23,36 @@
 #define LED_PWM_ALT_FN		0
 #define LED_PWM_PIN		PIN(A, 3)
 
-#define COLOR_LEVELS		256
 #define EFFECT_TIMEOUT		5
 
 #define LED_PWM_FREQ		4000000
+
+/* Without gamma correction we are operating at 256 color channel levels.
+ * The timer interrupt is generated with frequency 960.000 Hz / 20 = 48.000 Hz,
+ * and there are three color channels, so one entire color level frame is sent
+ * with frequency 48.000 Hz / 3 = 16.000 Hz.
+ *
+ * With 256 color levels we are sending 255 color level frames [1], thus the
+ * different intensity levels are generated by software PWM, which blinks at
+ * frequency 16.000 Hz / 255 =~ 62.7 Hz.
+ *
+ * With gamma correction we have different color levels and so are working at
+ * different frequencies to achieve the blinking at approximately 60 Hz.
+ * Gamma correction parameters are platform specific, defined later in this
+ * file.
+ *
+ * [1] Why are we sending a sequence of 255 frames for 256 different levels?
+ *     Consider if the levels were only 4, then we would need to send only
+ *     sequences of length 3:
+ *                sequence
+ *       level 0: 0, 0, 0
+ *       level 1: 1, 0, 0
+ *       level 2: 1, 1, 0
+ *       level 3: 1, 1, 1
+ */
+#define LED_TIMER_FREQ		960000
+#define LED_TIMER_PERIOD	5
+#define COLOR_LEVELS		256
 
 enum {
 	RED = 0,
@@ -76,48 +102,115 @@ bool effect_reset_finished;
 static uint8_t pwm_brightness;
 
 static bool gamma_correction;
+static uint16_t color_levels;
 
-static const uint8_t gamma_table_r[256] = {
-	  0,   0,   0,   0,   0,   1,   1,   1,   1,   1,   1,   1,   1,   1,   2,   2,
-	  2,   2,   2,   2,   2,   2,   2,   3,   3,   3,   3,   3,   3,   3,   3,   4,
-	  4,   4,   4,   4,   4,   5,   5,   5,   5,   5,   6,   6,   6,   6,   6,   7,
-	  7,   7,   7,   8,   8,   8,   8,   9,   9,   9,  10,  10,  10,  10,  11,  11,
-	 11,  12,  12,  12,  13,  13,  13,  14,  14,  15,  15,  15,  16,  16,  17,  17,
-	 17,  18,  18,  19,  19,  20,  20,  21,  21,  22,  22,  23,  23,  24,  24,  25,
-	 25,  26,  26,  27,  28,  28,  29,  29,  30,  31,  31,  32,  32,  33,  34,  34,
-	 35,  36,  37,  37,  38,  39,  39,  40,  41,  42,  43,  43,  44,  45,  46,  47,
-	 47,  48,  49,  50,  51,  52,  53,  54,  54,  55,  56,  57,  58,  59,  60,  61,
-	 62,  63,  64,  65,  66,  67,  68,  70,  71,  72,  73,  74,  75,  76,  77,  79,
-	 80,  81,  82,  83,  85,  86,  87,  88,  90,  91,  92,  94,  95,  96,  98,  99,
-	100, 102, 103, 105, 106, 108, 109, 110, 112, 113, 115, 116, 118, 120, 121, 123,
-	124, 126, 128, 129, 131, 132, 134, 136, 138, 139, 141, 143, 145, 146, 148, 150,
-	152, 154, 155, 157, 159, 161, 163, 165, 167, 169, 171, 173, 175, 177, 179, 181,
-	183, 185, 187, 189, 191, 193, 196, 198, 200, 202, 204, 207, 209, 211, 214, 216,
-	218, 220, 223, 225, 228, 230, 232, 235, 237, 240, 242, 245, 247, 250, 252, 255,
+#if defined(STM32F030X8) && SYS_CORE_FREQ == 48000000U
+/* On STM32 (Cortex-M0 at 48 MHz) we have 640 color levels with gamma correction
+ * enabled.
+ *
+ * Since 640 = 256 * 2.5, the frames are sent 2.5 times faster, every 400 clock
+ * cycles (compare with every 1000 clock cycles with gamma correction disabled).
+ *
+ * On average, the interrupt takes around 163 clock cycles, so this should be
+ * fine.
+ */
+#define LED_TIMER_PERIOD_GC	8
+#define COLOR_LEVELS_GC		640
+
+static const uint16_t gamma_table_r[256] = {
+	  0,   1,   1,   1,   1,   1,   2,   2,   2,   2,   3,   3,   3,   4,   4,   4,
+	  4,   5,   5,   5,   6,   6,   6,   6,   7,   7,   7,   8,   8,   8,   9,   9,
+	 10,  10,  10,  11,  11,  12,  12,  13,  13,  14,  14,  15,  15,  16,  16,  17,
+	 17,  18,  18,  19,  20,  20,  21,  22,  22,  23,  24,  25,  25,  26,  27,  28,
+	 28,  29,  30,  31,  32,  33,  34,  35,  35,  36,  37,  38,  39,  40,  41,  42,
+	 44,  45,  46,  47,  48,  49,  50,  52,  53,  54,  55,  57,  58,  59,  60,  62,
+	 63,  65,  66,  67,  69,  70,  72,  73,  75,  77,  78,  80,  81,  83,  85,  86,
+	 88,  90,  92,  93,  95,  97,  99, 101, 103, 105, 107, 109, 111, 113, 115, 117,
+	119, 121, 123, 125, 127, 130, 132, 134, 136, 139, 141, 144, 146, 148, 151, 153,
+	156, 158, 161, 164, 166, 169, 171, 174, 177, 180, 183, 185, 188, 191, 194, 197,
+	200, 203, 206, 209, 212, 215, 218, 222, 225, 228, 231, 235, 238, 241, 245, 248,
+	252, 255, 259, 262, 266, 269, 273, 277, 280, 284, 288, 292, 296, 300, 304, 308,
+	311, 316, 320, 324, 328, 332, 336, 340, 345, 349, 353, 358, 362, 367, 371, 376,
+	380, 385, 389, 394, 399, 404, 408, 413, 418, 423, 428, 433, 438, 443, 448, 453,
+	458, 464, 469, 474, 480, 485, 490, 496, 501, 507, 512, 518, 524, 529, 535, 541,
+	547, 552, 558, 564, 570, 576, 582, 589, 595, 601, 607, 613, 620, 626, 633, 639,
 };
 
-static const uint8_t gamma_table_gb[256] = {
-	  0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   1,   1,   1,   1,   1,   1,
-	  1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   2,   2,   2,   2,
-	  2,   2,   2,   2,   2,   2,   2,   2,   2,   3,   3,   3,   3,   3,   3,   3,
-	  3,   3,   3,   4,   4,   4,   4,   4,   4,   4,   4,   5,   5,   5,   5,   5,
-	  5,   5,   6,   6,   6,   6,   6,   6,   7,   7,   7,   7,   7,   8,   8,   8,
-	  8,   8,   9,   9,   9,   9,   9,  10,  10,  10,  10,  11,  11,  11,  11,  12,
-	 12,  12,  12,  13,  13,  13,  14,  14,  14,  14,  15,  15,  15,  16,  16,  16,
-	 17,  17,  17,  18,  18,  18,  19,  19,  19,  20,  20,  20,  21,  21,  22,  22,
-	 22,  23,  23,  24,  24,  24,  25,  25,  26,  26,  27,  27,  27,  28,  28,  29,
-	 29,  30,  30,  31,  31,  32,  32,  33,  33,  34,  34,  35,  35,  36,  36,  37,
-	 38,  38,  39,  39,  40,  40,  41,  42,  42,  43,  43,  44,  45,  45,  46,  47,
-	 47,  48,  49,  49,  50,  51,  51,  52,  53,  53,  54,  55,  56,  56,  57,  58,
-	 58,  59,  60,  61,  62,  62,  63,  64,  65,  66,  66,  67,  68,  69,  70,  71,
-	 71,  72,  73,  74,  75,  76,  77,  78,  79,  79,  80,  81,  82,  83,  84,  85,
-	 86,  87,  88,  89,  90,  91,  92,  93,  94,  95,  96,  97,  98,  99, 100, 102,
-	103, 104, 105, 106, 107, 108, 109, 111, 112, 113, 114, 115, 116, 118, 119, 120,
+static const uint16_t gamma_table_gb[256] = {
+	  0,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   1,   2,   2,   2,   2,
+	  2,   2,   2,   2,   3,   3,   3,   3,   3,   3,   3,   4,   4,   4,   4,   4,
+	  4,   5,   5,   5,   5,   5,   6,   6,   6,   6,   7,   7,   7,   7,   8,   8,
+	  8,   8,   9,   9,   9,  10,  10,  10,  11,  11,  11,  12,  12,  12,  13,  13,
+	 13,  14,  14,  15,  15,  15,  16,  16,  17,  17,  18,  18,  18,  19,  19,  20,
+	 20,  21,  21,  22,  23,  23,  24,  24,  25,  25,  26,  27,  27,  28,  28,  29,
+	 30,  30,  31,  32,  32,  33,  34,  34,  35,  36,  37,  37,  38,  39,  40,  41,
+	 41,  42,  43,  44,  45,  46,  46,  47,  48,  49,  50,  51,  52,  53,  54,  55,
+	 56,  57,  58,  59,  60,  61,  62,  63,  64,  65,  66,  67,  69,  70,  71,  72,
+	 73,  74,  76,  77,  78,  79,  81,  82,  83,  84,  86,  87,  88,  90,  91,  92,
+	 94,  95,  97,  98, 100, 101, 103, 104, 106, 107, 109, 110, 112, 113, 115, 116,
+	118, 120, 121, 123, 125, 126, 128, 130, 132, 133, 135, 137, 139, 141, 143, 144,
+	146, 148, 150, 152, 154, 156, 158, 160, 162, 164, 166, 168, 170, 172, 174, 176,
+	179, 181, 183, 185, 187, 189, 192, 194, 196, 199, 201, 203, 206, 208, 210, 213,
+	215, 218, 220, 223, 225, 228, 230, 233, 235, 238, 241, 243, 246, 248, 251, 254,
+	257, 259, 262, 265, 268, 271, 273, 276, 279, 282, 285, 288, 291, 294, 297, 300,
 };
+#elif defined(GD32F1x0) && SYS_CORE_FREQ == 72000000U
+/* On GD32 (Cortex-M3 at 72 MHz) we have 1024 color levels with gamma correction
+ * enabled.
+ *
+ * Since 1024 = 256 * 4, the frames are sent 4 times faster, every 375 clock
+ * cycles (compare with every 1400 clock cycles with gamma correction disabled).
+ *
+ * On average, the interrupt takes around 138 clock cycles, so this should be
+ * fine.
+ */
+#define LED_TIMER_PERIOD_GC	5
+#define COLOR_LEVELS_GC		1024
+
+static const uint16_t gamma_table_r[256] = {
+	  0,   1,   1,   1,   2,   2,   3,   3,   4,   4,   4,   5,   5,    6,    6,    7,
+	  7,   8,   8,   8,   9,   9,  10,  10,  11,  11,  12,  12,  13,   13,   14,   15,
+	 15,  16,  17,  17,  18,  19,  19,  20,  21,  22,  22,  23,  24,   25,   26,   27,
+	 28,  29,  30,  31,  32,  33,  34,  35,  36,  37,  38,  39,  40,   42,   43,   44,
+	 45,  47,  48,  50,  51,  52,  54,  55,  57,  58,  60,  61,  63,   65,   66,   68,
+	 70,  71,  73,  75,  77,  79,  81,  83,  84,  86,  88,  90,  93,   95,   97,   99,
+	101, 103, 106, 108, 110, 113, 115, 118, 120, 123, 125, 128, 130,  133,  136,  138,
+	141, 144, 147, 149, 152, 155, 158, 161, 164, 167, 171, 174, 177,  180,  183,  187,
+	190, 194, 197, 200, 204, 208, 211, 215, 218, 222, 226, 230, 234,  237,  241,  245,
+	249, 254, 258, 262, 266, 270, 275, 279, 283, 288, 292, 297, 301,  306,  311,  315,
+	320, 325, 330, 335, 340, 345, 350, 355, 360, 365, 370, 376, 381,  386,  392,  397,
+	403, 408, 414, 420, 425, 431, 437, 443, 449, 455, 461, 467, 473,  480,  486,  492,
+	499, 505, 512, 518, 525, 532, 538, 545, 552, 559, 566, 573, 580,  587,  594,  601,
+	609, 616, 624, 631, 639, 646, 654, 662, 669, 677, 685, 693, 701,  709,  717,  726,
+	734, 742, 751, 759, 768, 776, 785, 794, 802, 811, 820, 829, 838,  847,  857,  866,
+	875, 885, 894, 903, 913, 923, 932, 942, 952, 962, 972, 982, 992, 1002, 1013, 1023,
+};
+
+static const uint16_t gamma_table_gb[256] = {
+	  0,   0,   0,   1,   1,   1,   1,   1,   2,   2,   2,   2,   3,   3,   3,   3,
+	  3,   4,   4,   4,   4,   4,   5,   5,   5,   5,   6,   6,   6,   6,   7,   7,
+	  7,   7,   8,   8,   8,   9,   9,   9,  10,  10,  11,  11,  11,  12,  12,  13,
+	 13,  13,  14,  14,  15,  15,  16,  16,  17,  17,  18,  18,  19,  20,  20,  21,
+	 21,  22,  23,  23,  24,  25,  25,  26,  27,  27,  28,  29,  30,  30,  31,  32,
+	 33,  34,  34,  35,  36,  37,  38,  39,  40,  41,  42,  43,  44,  45,  46,  47,
+	 48,  49,  50,  51,  52,  53,  54,  55,  56,  58,  59,  60,  61,  62,  64,  65,
+	 66,  68,  69,  70,  72,  73,  74,  76,  77,  79,  80,  82,  83,  85,  86,  88,
+	 89,  91,  93,  94,  96,  98,  99, 101, 103, 104, 106, 108, 110, 112, 114, 115,
+	117, 119, 121, 123, 125, 127, 129, 131, 133, 135, 137, 140, 142, 144, 146, 148,
+	150, 153, 155, 157, 160, 162, 164, 167, 169, 172, 174, 177, 179, 182, 184, 187,
+	189, 192, 195, 197, 200, 203, 206, 208, 211, 214, 217, 220, 223, 226, 228, 231,
+	234, 238, 241, 244, 247, 250, 253, 256, 259, 263, 266, 269, 273, 276, 279, 283,
+	286, 290, 293, 297, 300, 304, 307, 311, 315, 318, 322, 326, 330, 333, 337, 341,
+	345, 349, 353, 357, 361, 365, 369, 373, 377, 381, 386, 390, 394, 398, 403, 407,
+	411, 416, 420, 425, 429, 434, 438, 443, 448, 452, 457, 462, 467, 471, 476, 481,
+};
+#else
+#error "LED driver gamma correction parameters not implemented for this platform"
+#endif
 
 static void _led_set_levels(unsigned led)
 {
-	uint8_t r, g, b;
+	uint16_t r, g, b;
 	unsigned idx;
 
 	r = leds[led].color.r;
@@ -181,6 +274,14 @@ void led_driver_set_gamma_correction(bool on)
 
 	gamma_correction = on;
 
+	if (on) {
+		color_levels = COLOR_LEVELS_GC;
+		timer_set_period(LED_TIMER, LED_TIMER_PERIOD_GC);
+	} else {
+		color_levels = COLOR_LEVELS;
+		timer_set_period(LED_TIMER, LED_TIMER_PERIOD);
+	}
+
 	for (int led = 0; led < LED_COUNT; ++led)
 		_led_set_levels(led);
 }
@@ -206,7 +307,7 @@ static uint16_t led_driver_prepare_data(unsigned channel)
 	 *
 	 * This saves some precious processor ticks. GCC with -Os should compile
 	 * the interrupt handler (together with exception entry and exit) to
-	 * something around 160 ticks for Cortex-M0, or 135 ticks for Cortex-M3
+	 * something around 163 ticks for Cortex-M0, or 138 ticks for Cortex-M3
 	 * (on average).
 	 */
 	static const uint32_t mask = BIT(31) | BIT(15),
@@ -229,7 +330,7 @@ static uint16_t led_driver_prepare_data(unsigned channel)
 	if (channel == BLUE) {
 		level2x += one;
 
-		if ((level2x & 0xffff) == COLOR_LEVELS)
+		if ((level2x & 0xffff) >= color_levels)
 			level2x = one;
 	}
 
@@ -284,6 +385,8 @@ void led_driver_config(void)
 	leds_states_default = 0;
 	leds_states_user = LED_BITS_ALL;
 	leds_states = 0;
+	gamma_correction = false;
+	color_levels = gamma_correction ? COLOR_LEVELS_GC : COLOR_LEVELS;
 
 	led_set_color(LED_COUNT, WHITE_COLOR);
 
@@ -303,7 +406,9 @@ void led_driver_config(void)
 	led_driver_set_brightness(100);
 
 	/* Initialize timer (every tick we send one frame) */
-	timer_init(LED_TIMER, timer_interrupt, 50, 2400000, 4);
+	timer_init(LED_TIMER, timer_interrupt,
+		   gamma_correction ? LED_TIMER_PERIOD_GC : LED_TIMER_PERIOD,
+		   LED_TIMER_FREQ, 4);
 	timer_enable(LED_TIMER, true);
 
 	/* Configure boot effect */

commit c6d17888f1c391a71f1aa264f5bd9b87a973404b

drivers/led_driver: Fix LED_TIMER_PERIOD value
Commit d584227619bd ("drivers/led_driver: Use more color levels when
gamma correcting") introduced wrong value of 5 for LED_TIMER_PERIOD,
resulting in the led_driver_send_frame() interrupt to be called too fast
when gamma correction is disabled. On STM32, for example, the interrupt
is called every 250 cycles instead of every 1000, as described by the
comments added in the same commit.

Fix LED_TIMER_PERIOD definition.

Fixes: d584227619bd ("drivers/led_driver: Use more color levels when gamma correcting")
Signed-off-by: Marek Behún <marek.behun@nic.cz>

src/drivers/led_driver.c
@@ -33,7 +33,7 @@
  *       level 3: 1, 1, 1
  */
 #define LED_TIMER_FREQ		960000
-#define LED_TIMER_PERIOD	5
+#define LED_TIMER_PERIOD	20
 #define COLOR_LEVELS		256
 
 enum {